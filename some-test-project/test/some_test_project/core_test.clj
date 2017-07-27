7cc913ce29af8cbdd708e64f8e895b12b244f846
DimaYasenko <dima.yasenko@gmail.com>
2017-07-27 15:22:58 +0100

    add test project for testing Circle CI

---
+++ some-test-project/test/some_test_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns some-test-project.core-test
+  (:require [clojure.test :refer :all]
+            [some-test-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
